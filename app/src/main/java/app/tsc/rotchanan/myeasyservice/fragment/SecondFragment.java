a97dc1247d3b05d49e7ba3f439d0629fbc965ae0
Rotchanan.s <rotchananhoshi@gmail.com>
2017-10-22 16:00:41 +0700

    Finish 22Oct17

--- app/src/main/java/app/tsc/rotchanan/myeasyservice/fragment/SecondFragment.java
+++ app/src/main/java/app/tsc/rotchanan/myeasyservice/fragment/SecondFragment.java
@@ -1,5 +1,9 @@
 package app.tsc.rotchanan.myeasyservice.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import app.tsc.rotchanan.myeasyservice.R;
 
@@ -16,6 +22,8 @@ import app.tsc.rotchanan.myeasyservice.R;
 
 public class SecondFragment extends Fragment {
 
+    private ImageView imageView;
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -24,7 +32,51 @@ public class SecondFragment extends Fragment {
         backController();
 
 
+        // Image Controller
+        imageController();
+
+
     } // Main method
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // requestCode : เหมือนตั๋วบอกว่าต้อง return ไปที่ไหน
+        // resultCode : คือผลการทำงาน
+        // data : ข้อมูลที่ return กลับมา
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            try {
+
+                Uri uri = data.getData(); //Uri คือตัวสำหรับคัดกรองว่าเป็นภาพ,เสียง หรือวิดิโอ เป็นต้น
+                Bitmap bitmap = BitmapFactory.decodeStream(
+                        getActivity().getContentResolver().openInputStream(uri));
+
+                imageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            Toast.makeText(getActivity(), "Not choose Image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void imageController() {
+        imageView = getView().findViewById(R.id.imvHuman);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT); //คำสั่งสำหรับใช้ให้คนอื่นทำงาน
+                intent.setType("image/*"); //สั่งให้ intent ช่วยหาว่ามีแอพตัวไหนอ่านรูปภาพได้บ้าง ใน device นี้
+                startActivityForResult(intent.createChooser(intent,
+                        "Please Choose App"), 1); //คำสั่งสำหรับให้แสดง dialog ถาม user ให้เลือก app
+            }
+        });
+    }
 
     private void backController() {
         // initial
