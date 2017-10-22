b15fdd70b971e820250870f69dc61d63b4773eb6
Rotchanan.s <rotchananhoshi@gmail.com>
2017-10-22 14:44:06 +0700

    Hamburger Icon Finish

--- app/src/main/java/app/tsc/rotchanan/myeasyservice/MainActivity.java
+++ app/src/main/java/app/tsc/rotchanan/myeasyservice/MainActivity.java
@@ -1,17 +1,26 @@
 package app.tsc.rotchanan.myeasyservice;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import app.tsc.rotchanan.myeasyservice.fragment.MainFragment;
+import app.tsc.rotchanan.myeasyservice.fragment.SecondFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +37,54 @@ public class MainActivity extends AppCompatActivity {
         textController();
 
 
+        // Create Toolbar
+        createToolbar();
+
+
     } // Main Method
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        actionBarDrawerToggle.syncState();
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+
+    }
+
+    private void createToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolBarMain);
+        setSupportActionBar(toolbar);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                drawerLayout,
+                R.string.open,
+                R.string.close
+        );
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
 
     private void textController() {
         TextView mainTextView = (TextView) findViewById(R.id.txtMainFragment);
@@ -39,6 +95,11 @@ public class MainActivity extends AppCompatActivity {
         mainTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentFragmentMain, new MainFragment())
+                        .commit();
+
 
                 // Close Drawer
                 drawerLayout.closeDrawers();
@@ -51,6 +112,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentFragmentMain, new SecondFragment())
+                        .commit();
+
+
                 // Close Drawer
                 drawerLayout.closeDrawers();
 
@@ -61,6 +127,8 @@ public class MainActivity extends AppCompatActivity {
         exitTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                finish();
 
                 // Close Drawer
                 drawerLayout.closeDrawers();
